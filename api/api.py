12dbadcb77a1fb04dd0a536ed0271e84631a8ce0
FireshipMe <mqclane@gmail.com>
2020-06-07 16:14:04 +0300

    API refactoring to hopefully fix losing connection to DB

--- api/api.py
+++ api/api.py
@@ -1,21 +1,26 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from database_interaction import getAllPizzas
+from database_setup import Pizza
+from config import DB_CREDENTIALS
+import json
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CREDENTIALS
+db = SQLAlchemy(app)
 CORS(app)
 api = Api(app)
 
 
-class Pizza(Resource):
+class PizzaApi(Resource):
     def get(self):
-        return getAllPizzas()
+        pizzas = db.session.query(Pizza).all()
+        return json.dumps([x.serialize for x in pizzas], ensure_ascii=False)
 
 
-api.add_resource(Pizza, '/api/listall')
+api.add_resource(PizzaApi, '/api/listall')
 
 if __name__ == "__main__":
     app.run()
